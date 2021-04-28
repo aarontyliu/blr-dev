ca796b410e37461e4170e4b82d632834b0e8bb1d
treetop-fish <aaron.ty.liu@gmail.com>
2021-04-28 12:00:03 -0400

    add original implementation of SGHMC

--- src/blr_sghmc/classifier.py
+++ src/blr_sghmc/classifier.py
@@ -3,49 +3,43 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.utils.multiclass import unique_labels
 from sklearn.utils.validation import check_array, check_is_fitted, check_X_y
 
-from blr_sghmc.optim import hmc, sghmc
+from blr_sghmc.optim import hmc, sghmc, original_sghmc
 
 
 class BayesianLogisticRegression(ClassifierMixin, BaseEstimator):
     """
     Bayesian Logistic Regression.
     """
+
     def __init__(
         self,
-        penalty="l2",
-        tol=1e-4,
         fit_intercept=True,
-        class_weight=None,
-        random_state=None,
-        solver="hmc",
+        solver="sghmc",
         max_iter=100,
-        multi_class="auto",
         verbose=0,
-        n_jobs=None,
         batch_size=64,
-        available_solvers=("hmc", "sghmc"),
     ):
 
-        self.penalty = penalty
-        self.tol = tol
         self.fit_intercept = fit_intercept
-        self.class_weight = class_weight
-        self.random_state = random_state
-        self.solver = solver
+        self.available_solvers = {
+            "hmc": hmc,
+            "sghmc": sghmc,
+            "original_sghmc": original_sghmc,
+        }
+        assert (
+            solver in self.available_solvers
+        ), f"Please use available solvers: {available_solvers.keys()}"
+        self.solver = self.available_solvers[solver]
         self.max_iter = max_iter
-        self.multi_class = multi_class
         self.verbose = verbose
-        self.n_jobs = n_jobs
         self.batch_size = batch_size
-        self.available_solvers = available_solvers
 
     def fit(self, X, y, M="I"):
 
         X, y = check_X_y(X, y)
 
         self.classes_ = unique_labels(y)
-        self.X_ = X
-        self.y_ = y
+        self.X_, self.y_ = X, y
 
         if self.fit_intercept:
             self.X_ = np.insert(self.X_, 0, 1, 1)
@@ -53,30 +47,16 @@ class BayesianLogisticRegression(ClassifierMixin, BaseEstimator):
         if M == "I":
             self.M = np.eye(self.X_.shape[1])
 
-        assert (
-            self.solver in self.available_solvers
-        ), f"Please use available solvers: {available_solvers}"
-        if self.solver == "hmc":
-            self.parameters_ = hmc(
-                self.X_,
-                self.y_,
-                self.M,
-                max_iter=self.max_iter,
-                m=400,
-                eps=1e-4,
-                verbose=self.verbose,
-            )
-        elif self.solver == "sghmc":
-            self.parameters_ = sghmc(
-                self.X_,
-                self.y_,
-                self.M,
-                batch_size=self.batch_size,
-                max_iter=self.max_iter,
-                m=100,
-                eps=1e-3,
-                verbose=self.verbose,
-            )
+        self.parameters_ = self.solver(
+            self.X_,
+            self.y_,
+            self.M,
+            max_iter=self.max_iter,
+            m=400,
+            eps=1e-4,
+            verbose=self.verbose,
+        )
+
         return self
 
     def predict_proba(self, X):
--- src/blr_sghmc/optim.py
+++ src/blr_sghmc/optim.py
@@ -3,6 +3,9 @@ from numpy.random import multivariate_normal
 
 
 def sigmoid(x):
+    """
+    Function returns
+    """
     return 1 / (1 + np.exp(-x))
 
 
@@ -80,31 +83,69 @@ def hmc(X, y, M, penalty="l1", max_iter=500, m=1000, eps=1e-5, verbose=False):
     return est_params
 
 
-def sghmc(
-    X, y, M, batch_size=16, penalty="l1", max_iter=500, m=1000, eps=1e-5, verbose=False
+def original_sghmc(
+    X, y, M, batch_size=16, max_iter=500, m=1000, eps=1e-5, verbose=False
 ):
     n_samples, n_params = X.shape[0], X.shape[1]
     scale = n_samples / batch_size
-    params = np.random.normal(size=n_params).reshape(-1, 1)
+    params_t = np.random.normal(size=n_params).reshape(-1, 1)
     M_inv = np.linalg.inv(M)
-    rs = multivariate_normal(np.zeros(n_params), M, size=(max_iter, 1))
     batch_ids = np.random.choice(range(n_samples), size=(max_iter, batch_size))
+    for t in range(max_iter):
+        r = np.random.normal(size=(1, n_params))
+        batch_X, batch_y = X[batch_ids[t]], y[batch_ids[t]]
+        params_0 = params_t.copy()
+        for _ in range(m):
+            params_0 += eps * (M_inv @ r.T)
+            B_hat = 1 / 2 * eps * V(batch_X, batch_y, params_0)
+            C = B_hat
+            r -= (
+                eps * del_U(batch_X, batch_y, params_0, scale=scale)
+                + eps * (C @ (M_inv @ r.T)).flatten()
+                - multivariate_normal(np.zeros(n_params), 2 * (C - B_hat) * eps)
+            )
+        if verbose:
+            if not t % 100:
+                print(
+                    f"Iteration {t:>5}"
+                    + "".join(f"| {p:>7.4f}" for p in params.flatten())
+                )
+        params_t = params_0.copy()
+    return params_t
+
+
+def sghmc(X, y, M, batch_size=16, max_iter=500, m=1000, eps=1e-5, verbose=False):
+    n_samples, n_params = X.shape[0], X.shape[1]
+    scale = n_samples / batch_size
+    params = np.random.normal(size=n_params).reshape(-1, 1)
+    skip = np.allclose(M, np.eye(n_params))
+    if not skip:
+        M_inv = np.linalg.inv(M)
+    zeros = np.zeros(n_params)
+    rs = multivariate_normal(zeros, M, size=(max_iter, 1))
+    batch_ids = np.random.choice(range(n_samples), size=(max_iter, batch_size))
+
     for t in range(max_iter):
         batch_X, batch_y = X[batch_ids[t]], y[batch_ids[t]]
         for _ in range(m):
-            params += eps * (M_inv @ rs[t].T)
+            if not skip:
+                params += eps * (M_inv @ rs[t].T)
+            else:
+                params += eps * rs[t].T
+
             B_hat = 1 / 2 * eps * V(batch_X, batch_y, params)
             C = B_hat
-            if eps > 1e-3:
+            if not skip:
                 rs[t] -= (
                     eps * del_U(batch_X, batch_y, params, scale=scale)
                     + eps * (C @ (M_inv @ rs[t].T)).flatten()
-                    - multivariate_normal(np.zeros(n_params), 2 * (C - B_hat) * eps)
+                    - multivariate_normal(zeros, 2 * (C - B_hat) * eps)
                 )
             else:
                 rs[t] -= (
-                    eps * del_U(batch_X, batch_y, params, scale=n_samples / batch_size)
-                    + eps * (C @ (M_inv @ rs[t].T)).flatten()
+                    eps * del_U(batch_X, batch_y, params, scale=scale)
+                    + eps * (C @ rs[t].T).flatten()
+                    - multivariate_normal(zeros, 2 * (C - B_hat) * eps)
                 )
         if verbose:
             if not t % 100:
